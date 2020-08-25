8238f14acfa164f2f44a8baf1da921224b914dee
Bruno <brunof.tech@gmail.com>
2020-08-25 03:21:42 -0300

    editing main #1app

--- #1_App_IncDecPeople/contador_de_pessoas/lib/main.dart
+++ #1_App_IncDecPeople/contador_de_pessoas/lib/main.dart
@@ -3,6 +3,49 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",  
-    home: Container(color: Colors.white,)
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Pessoas: 0",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0), // in all sides
+              child: FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () {
+
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0), // in all sides
+              child: FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () {
+                  
+                },
+              ),
+            ),
+          ],
+        ),
+        Text(
+          "Pode Entrar!",
+          style: TextStyle(
+            color: Colors.white
+          ),
+        )
+      ],
+    )
   ));
 }
