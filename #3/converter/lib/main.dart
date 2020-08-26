1e8cbe39d310dde0289b5a5ab4106ed426e99214
Bruno <brunof.tech@gmail.com>
2020-08-26 01:42:07 -0300

    .

--- #3/converter/lib/main.dart
+++ #3/converter/lib/main.dart
@@ -1,13 +1,15 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 import "package:http/http.dart " as http;
 import "package:async/async.dart " as async;
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?key=6fe065c7";
 
-void main(){
+void main() async{
 
-  http.Response response = http.get(request);
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
 
   runApp(MaterialApp(
     home: Container()
