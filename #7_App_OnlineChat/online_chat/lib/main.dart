505eade9e3a7a80a5c73fe930da9d965718e8820
Bruno <brunof.tech@gmail.com>
2020-08-28 03:28:31 -0300

    listen changes in db

--- #7_App_OnlineChat/online_chat/lib/main.dart
+++ #7_App_OnlineChat/online_chat/lib/main.dart
@@ -5,7 +5,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 void main() async{
   runApp(MyApp());
 
-  Firestore.instance.collection("col").document("doc").setData({"texto":"Bruno"}); //Padrão signton??
+  QuerySnapshot snapshot = await Firestore.instance.collection("mensagens").getDocuments();
+  snapshot.documents.forEach((d){
+    print(d.data);
+  });
+
+  Firestore.instance.collection("mensagens").snapshots().listen((dado) {
+    dado.documents.forEach((d){
+      print(d.data);
+    });
+  });
+
 
 }
 class MyApp extends StatelessWidget {
