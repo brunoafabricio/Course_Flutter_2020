959cfb6bb3a78cc0e48015f9fce0b52ca30e901f
Bruno <brunof.tech@gmail.com>
2020-09-12 00:50:27 -0300

    how pageview work

--- #8_App_VirtualStore/virtualstore/lib/screens/home_screen.dart
+++ #8_App_VirtualStore/virtualstore/lib/screens/home_screen.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
+
+    //_pageController.animateToPage(page, duration: null, curve: null) change page
+
     return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(), // block swipe to side
+      children: <Widget>[
+        Container(color: Colors.yellow,),
+        Container(color: Colors.red,),
+        Container(color: Colors.green,),
+      ]
       
     );
   }
